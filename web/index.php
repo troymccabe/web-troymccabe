eb64f93b3600c92e30b8ea52b9e995c2e16c61b6
Troy McCabe <troy@dydro.com>
2013-04-04 22:42:24 -0500

    Error reporting

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+ini_set('display_errors', 'on');
+
 $app = new Silex\Application();
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
